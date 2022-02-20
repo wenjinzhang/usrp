4f5471585c5af6a4b7f15e6aea1871f6f5e85543
wenjinzhang <44008219+wenjinzhang@users.noreply.github.com>
2022-02-20 15:44:21 -0500

    update single tone

--- tx.py
+++ tx.py
@@ -1,9 +1,21 @@
 import uhd
 import numpy as np
+import time
+
 usrp = uhd.usrp.MultiUSRP()
 samples = 0.1*np.random.randn(10000) + 0.1j*np.random.randn(10000) # create random signal
-duration = 10 # seconds
-center_freq = 915e6
+duration = 30 # seconds
+center_freq = 5e9 - 312500*10 
+
 sample_rate = 1e6
-gain = 20 # [dB] start low then work your way up
-usrp.send_waveform(samples, duration, center_freq, sample_rate, [0], gain)
+gain = 60 # [dB] start low then work your way up
+start_time = time.time()
+
+time.sleep(40)
+
+norm_gains = [0.80, 0.90, 1]
+gains = [x*90 for x in norm_gains]
+for gain in gains:
+    print("--------start to send:{}---------------------------".format(time.time()-start_time))
+    usrp.send_waveform(samples, duration, center_freq, sample_rate, [0], gain)
+    print("---------sent at {} gain-------------".format(gain))
